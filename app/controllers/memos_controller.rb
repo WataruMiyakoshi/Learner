79bdb65ba262f37f71b5bf4f3ad59ee72c160f15
WataruMiyakoshi <67892455+WataruMiyakoshi@users.noreply.github.com>
2020-07-29 20:49:46 +0900

    PostMemos

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,13 +1,25 @@
 class MemosController < ApplicationController
   before_action :move_to_sign_in
+  before_action :set_memo
 
   def index
-  end
+  # @memos = Memo.all
+
+  # binding.pry
+end
 
   def new
+    @memo = Memo.new
   end
 
   def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    end
+    # else
+    #   redirect_to new_memo_path
+    # end
   end
 
   def show
@@ -23,4 +35,13 @@ def move_to_sign_in
   unless user_signed_in?
     redirect_to '/users/sign_in'
   end
+end
+
+def memo_params
+  params.require(:memo).permit(:title, :body, :image).merge(user_id: current_user.id)
+end
+
+def set_memo
+  # @memos = Memo.find_by(user_id: current_user.id)
+  @memos = Memo.all
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :memos
+  # has_many :memos
   belongs_to :user
 end
