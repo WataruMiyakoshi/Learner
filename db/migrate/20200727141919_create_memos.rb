3d649cc785ffd4a16d33588121c353677fe3460d
WataruMiyakoshi <67892455+WataruMiyakoshi@users.noreply.github.com>
2020-07-29 09:31:46 +0900

    NoSign_inUserRedirectToSign_in

--- db/migrate/20200727141919_create_memos.rb
+++ db/migrate/20200727141919_create_memos.rb
@@ -1,6 +1,7 @@
 class CreateMemos < ActiveRecord::Migration[6.0]
   def change
     create_table :memos do |t|
+      t.string :title
       t.text :body
       t.string :image
       t.references :user, null: false, foreign_key: true
